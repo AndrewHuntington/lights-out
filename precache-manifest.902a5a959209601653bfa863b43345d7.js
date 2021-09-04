432b6cb3ce0f46c5c0d49e46530f14affce242ba
Andrew Huntington <andrew.huntington@gmail.com>
2021-09-05 00:53:52 +0900

    Updates

---
+++ precache-manifest.902a5a959209601653bfa863b43345d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b471b41e2304a14768a7",
+    "url": "/lights-out/static/css/main.1227ac37.chunk.css"
+  },
+  {
+    "revision": "b471b41e2304a14768a7",
+    "url": "/lights-out/static/js/main.b471b41e.chunk.js"
+  },
+  {
+    "revision": "a989bfac1d920b4f2190",
+    "url": "/lights-out/static/js/1.a989bfac.chunk.js"
+  },
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "b373e333d20f9777cf7ce899e161d78e",
+    "url": "/lights-out/index.html"
+  }
+];
